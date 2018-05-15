1c5df1d5a4828976ec6a5744ce84661535435470
hanss314 <24215006+hanss314@users.noreply.github.com>
2018-05-15 10:39:03 -0700

    More operations

--- operations.c
+++ operations.c
@@ -10,33 +10,153 @@
 char* op_str[] = {
     "+",
     "-",
+    "*",
+    "/",
+    "**",
+    "%",
+    "abs",
+
+    "&",
+    "|",
+    "^",
+    "~",
+    "<<",
+    ">>",
+    
+    "==",
+    "!=",
+    "<=",
+    "<",
+    ">=",
+    ">"
+
     "rot",
     "p",
     "eval",
     "if",
+    "swp",
 };
 
 void (*op_func[]) (struct StackNode **) = {
     &apush_add,
     &apush_sub,
+    &apush_prod,
+    &apush_div,
+    &apush_pow,
+    &apush_mod,
+    &apush_abs,
+    
+    &apush_and,     
+    &apush_or,
+    &apush_xor,
+    &apush_comp,
+    &apush_shiftl,
+    &apush_shiftr,
+    
+    &apush_eq,
+    &apush_neq,
+    &apush_leq,
+    &apush_ltn,
+    &apush_geq,
+    &apush_gtn,
+
     &apush_rot,
     &apush_print_int,
     &apush_eval,
     &apush_cond,
+    &apush_swap,
 };
 
 int apush_num_ops() {
     return sizeof(op_str) / sizeof(char *);
 }
+
+//logical
+void apush_eq(struct StackNode** stack){
+    int a = pop_int(stack);
+    push_int(stack, pop_int(stack) == a);
+}
+void apush_neq(struct StackNode** stack){
+    int a = pop_int(stack);
+    push_int(stack, pop_int(stack) != a);
+}
+void apush_leq(struct StackNode** stack){
+    int a = pop_int(stack);
+    push_int(stack, pop_int(stack) <= a);
+}
+void apush_geq(struct StackNode** stack){
+    int a = pop_int(stack);
+    push_int(stack, pop_int(stack) >= a);
+}
+void apush_ltn(struct StackNode** stack){
+    int a = pop_int(stack);
+    push_int(stack, pop_int(stack) < a);
+}
+void apush_gtn(struct StackNode** stack){
+    int a = pop_int(stack);
+    push_int(stack, pop_int(stack) > a);
+}
+
+//bitwise
+void apush_and(struct StackNode** stack){
+    push_int(stack, pop_int(stack) & pop_int(stack));
+}
+void apush_or(struct StackNode** stack){
+    push_int(stack, pop_int(stack) | pop_int(stack));
+}
+void apush_xor(struct StackNode** stack){
+    push_int(stack, pop_int(stack) ^ pop_int(stack));
+}
+void apush_comp(struct StackNode** stack){
+    push_int(stack, ~pop_int(stack));
+}
+void apush_shiftl(struct StackNode** stack){
+    int a = pop_int(stack);
+    push_int(stack, pop_int(stack) << a);
+}
+void apush_shiftr(struct StackNode** stack){
+    int a = pop_int(stack);
+    push_int(stack, pop_int(stack) >> a);
+}
+
+//arith
+void apush_abs(struct StackNode** stack){
+    int a = pop_int(stack);
+    if (a<0) a = -a;
+    push_int(stack, a);
+}
+void apush_prod(struct StackNode** stack){
+    push_int(stack, pop_int(stack) * pop_int(stack));
+}
+
+void apush_div(struct StackNode** stack){
+    int a = pop_int(stack);
+    push_int(stack, pop_int(stack) / a);
+}
+void apush_pow(struct StackNode** stack){
+    int exp = pop_int(stack), base = pop_int(stack), result = 1;
+    while(1){
+        if (exp & 1) result *= base;
+        exp >>= 1;
+        if (!exp) break;
+        base *= base;
+    }
+    push_int(stack, result);
+}
+void apush_mod(struct StackNode** stack){
+    int a = pop_int(stack);
+    push_int(stack, pop_int(stack) % a);
+}
 void apush_add(struct StackNode** stack){
     push_int(stack, pop_int(stack) + pop_int(stack));
 }
-
 void apush_sub(struct StackNode** stack){
     int a = pop_int(stack);
     push_int(stack, pop_int(stack) - a);
 }
 
+
+//misc stack
 void apush_rot(struct StackNode** stack){
     int rotations = pop_int(stack);
     int size = get_size(*stack);
@@ -54,6 +174,13 @@ void apush_eval(struct StackNode** stack){
     char* text = pop_str(stack);
     char** code = apush_split_line(text, 0)[0];
     execute_code(code, *stack);
+}
+
+void apush_swap(struct StackNode** stack){
+    union Data a = pop(stack);
+    union Data b = pop(stack);
+    push(stack, a);
+    push(stack, b);
 }
 
 void apush_cond(struct StackNode** stack){
--- operations.h
+++ operations.h
@@ -2,13 +2,34 @@
 
 void apush_add(struct StackNode**);
 void apush_sub(struct StackNode**);
+void apush_prod(struct StackNode**);
+void apush_div(struct StackNode**);
+void apush_pow(struct StackNode**);
+void apush_mod(struct StackNode**);
+void apush_add(struct StackNode**);
+void apush_abs(struct StackNode**);
+
+void apush_and(struct StackNode**);
+void apush_or(struct StackNode**);
+void apush_xor(struct StackNode**);
+void apush_comp(struct StackNode**);
+void apush_shiftl(struct StackNode**);
+void apush_shiftr(struct StackNode**);
+
+void apush_eq(struct StackNode**);
+void apush_neq(struct StackNode**);
+void apush_leq(struct StackNode**);
+void apush_ltn(struct StackNode**);
+void apush_geq(struct StackNode**);
+void apush_gtn(struct StackNode**);
+
 void apush_rot(struct StackNode**);
 void apush_print_int(struct StackNode**);
 void apush_eval(struct StackNode**);
 void apush_cond(struct StackNode**);
+void apush_swap(struct StackNode**);
+
 char* op_str[];
-
 void (*op_func[]) (struct StackNode **);
-
 int apush_num_ops();
 
